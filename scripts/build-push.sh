a10677e0612c2f6da3dc84b16aa22fcf93b0127f
Anton Petrov <anton.a.petrov@gmail.com>
2021-07-13 10:22:38 +0300

    patch

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -3,12 +3,12 @@
 set -e
 set -x
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USER" --password-stdin
 
-docker build -t tiangolo/node-frontend:14 .
+docker build -t antonapetrov/node-frontend:14 .
 
-docker build -t tiangolo/node-frontend:latest .
+docker build -t antonapetrov/node-frontend:latest .
 
-docker push tiangolo/node-frontend:14
+docker push antonapetrov/node-frontend:14
 
-docker push tiangolo/node-frontend:latest
+docker push antonapetrov/node-frontend:latest
